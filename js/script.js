b883fef109419f03350101524e77af35f5a3d092
Ronny Weiss <30749177+RonnyWeiss@users.noreply.github.com>
2021-08-31 21:58:02 +0200

    add feature to invert selection from feature request #1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,47 @@
-function apexCheckboxToggle(pSelf, pIconSelectall, pTextSelecAll, pIconUnselectAll, pTextUnselectAll) {
+function apexCheckboxToggle(pSelf, pIconSelectall, pTextSelecAll, pIconUnselectAll, pTextUnselectAll, pIconInvertSelection, pTextInvertSelection) {
     "use strict";
-    var scriptVersion = "1.0";
+    var util = {
+        featureDetails: {
+            name: "APEX Checkbox toggle Checked",
+            scriptVersion: "1.1",
+            utilVersion: "1.5",
+            url: "https://github.com/RonnyWeiss",
+            license: "MIT"
+        },
+        isDefinedAndNotNull: function (pInput) {
+            if (typeof pInput !== "undefined" && pInput !== null && pInput != "") {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    };
+
+    apex.debug.info({
+        "fct": util.featureDetails.name + " - " + "apexCheckboxToggle",
+        "arguments": {
+            "pSelf": pSelf,
+            "pIconSelectall": pIconSelectall,
+            "pTextSelecAll": pTextSelecAll,
+            "pIconUnselectAll": pIconUnselectAll,
+            "pTextUnselectAll": pTextUnselectAll,
+            "pIconInvertSelection": pIconInvertSelection,
+            "pTextInvertSelection": pTextInvertSelection
+        },
+        "featureDetails": util.featureDetails
+    });
+
+    function addIcon(pParent, pIcon) {
+        var sSpanIcon = $("<span></span>");
+        sSpanIcon.css("line-height", "inherit");
+        sSpanIcon.css("font-size", "inherit");
+        sSpanIcon.css("margin-right", "3px");
+        sSpanIcon.addClass("apex-checkbox-toggle-plug-in-icon");
+        sSpanIcon.attr("aria-hidden", "true");
+        sSpanIcon.addClass("fa");
+        sSpanIcon.addClass(pIcon);
+        $(pParent).prepend(sSpanIcon);
+    }
 
     $.each($(pSelf.affectedElements), function (i, item) {
 
@@ -11,6 +52,7 @@ function apexCheckboxToggle(pSelf, pIconSelectall, pTextSelecAll, pIconUnselectA
             // select all
             var sSpan = $("<span></span>");
             sSpan.css("margin-left", "20px");
+            sSpan.css("user-select", "none");
             sSpan.addClass("apex-checkbox-toggle-plug-in-label");
             sSpan.text(pTextSelecAll);
             sSpan.on("click", function () {
@@ -22,22 +64,15 @@ function apexCheckboxToggle(pSelf, pIconSelectall, pTextSelecAll, pIconUnselectA
             });
 
             if (pIconSelectall) {
-                var sSpanIcon = $("<span></span>");
-                sSpanIcon.css("line-height", "inherit");
-                sSpanIcon.css("font-size", "inherit");
-                sSpanIcon.css("margin-right", "3px");
-                sSpanIcon.addClass("apex-checkbox-toggle-plug-in-icon");
-                sSpanIcon.attr("aria-hidden", "true");
-                sSpanIcon.addClass("fa");
-                sSpanIcon.addClass(pIconSelectall);
-                $(sSpan).prepend(sSpanIcon);
+                addIcon(sSpan, pIconSelectall);
             }
 
             $("#" + itemID + "_LABEL").append(sSpan);
 
-            // unselect all
+            // select none
             var uSpan = $("<span></span>");
             uSpan.css("margin-left", "8px");
+            uSpan.css("user-select", "none");
             uSpan.addClass("apex-checkbox-toggle-plug-in-label");
             uSpan.text(pTextUnselectAll);
             uSpan.on("click", function () {
@@ -45,18 +80,50 @@ function apexCheckboxToggle(pSelf, pIconSelectall, pTextSelecAll, pIconUnselectA
             });
 
             if (pIconSelectall) {
-                var uSpanIcon = $("<span></span>");
-                uSpanIcon.css("line-height", "inherit");
-                uSpanIcon.css("font-size", "inherit");
-                uSpanIcon.css("margin-right", "3px");
-                uSpanIcon.addClass("apex-checkbox-toggle-plug-in-icon");
-                uSpanIcon.attr("aria-hidden", "true");
-                uSpanIcon.addClass("fa");
-                uSpanIcon.addClass(pIconUnselectAll);
-                $(uSpan).prepend(uSpanIcon);
+                addIcon(uSpan, pIconUnselectAll);
             }
 
             $("#" + itemID + "_LABEL").append(uSpan);
+
+            // invert selection
+            var iSpan = $("<span></span>");
+            iSpan.css("margin-left", "8px");
+            iSpan.css("user-select", "none");
+            iSpan.addClass("apex-checkbox-toggle-plug-in-label");
+            iSpan.text(pTextInvertSelection);
+            iSpan.on("click", function () {
+                var curVal = apex.item(itemID).getValue();
+                var curValArr = [];
+
+                if (util.isDefinedAndNotNull(curVal)) {
+
+                    if (Array.isArray(curVal)) {
+                        curValArr = curVal;
+                    }
+
+                    if (typeof curVal === "string") {
+                        curValArr = curVal.split(":");
+                    }
+                }
+
+                var arr = [];
+                $.each($(item).find(":checkbox"), function (idx, el) {
+                    arr.push($(el).attr("value"));
+                });
+
+                arr = arr.filter(function (arrEl) {
+                    return curValArr.indexOf(arrEl) < 0;
+                });
+
+                apex.item(itemID).setValue(arr.join(":"));
+
+            });
+
+            if (pIconSelectall) {
+                addIcon(iSpan, pIconInvertSelection);
+            }
+
+            $("#" + itemID + "_LABEL").append(iSpan);
         }
     });
 }
